05e025b3059e4330a23b123ec57261365dccafe3
秋 奈月 <43605695+akinazuki@users.noreply.github.com>
2024-06-14 21:14:29 +0900

    add dotenv

--- src/server/src/location.ts
+++ src/server/src/location.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
+import process from "node:process";
 
 const GOOGLE_APIKEY = process.env.GOOGLE_APIKEY;
 
-export async function searchLocation(args) {
+export async function searchLocation(args: any) {
   return await fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?${new URLSearchParams({
     ...args,
     key: GOOGLE_APIKEY,
