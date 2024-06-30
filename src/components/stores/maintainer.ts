d8cbd7f69e4fb4813f8589b27062881571b1e40f
秋 奈月 <43605695+akinazuki@users.noreply.github.com>
2024-06-30 22:35:45 +0900

    fix login

--- src/components/stores/maintainer.ts
+++ src/components/stores/maintainer.ts
@@ -16,10 +16,15 @@ export const userUserStore = defineStore("user", () => {
 
     const b64Data = userJWT.value.split(".")[1];
     const userData = JSON.parse(atob(b64Data));
-    return userData.payload;
+    return userData.payload as Maintainer;
   });
+  function setJWT(token: string) {
+    userJWT.value = token;
+  }
   return {
     maintainer,
     defaultAvatar: "https://vip2.loli.io/2022/10/04/k5NG4zLcdRZmTM6.png",
+    setJWT,
+    loggedIn: !!maintainer,
   };
 });
